5b3d2e152629754a46162d387252a2c5ab806d2d
Damien Cornu <damienc.cornu@gmail.com>
2011-09-27 19:23:58 +0200

    Preview
    Show loading while downloading informations.

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,28 @@ $(document).ready(function() {
 		'url' : 'https://graph.facebook.com/'+username+'/albums',
 		'dataType' : 'jsonp',
 		success : function (datas) {
-			var a=1,
+			var a=0, b=0,
+				htmlTemp = '',
 				albums = [];
+			
 			$.each(datas.data, function(i,item) {
+				
+				htmlTemp += '<div class="album album-'+item.id+'"><img src="img/ajax-loader.gif" alt="Loading..." style="margin-top:25px; margin-left:49px;"/><h3>'+item.name+'</h3></div>';
+				$("#albums").html(htmlTemp);
+				
 				$.ajax({
 					'type':'GET',
 					'url' : 'https://graph.facebook.com/'+item.id+'/photos',
 					'dataType' : 'jsonp',
 					success : function (otherdatas) {
+						albums=[];
 						var photos = [];
 						$.each(otherdatas.data, function(j,jtem) {
 								if(jtem.name != undefined) {photos.push('<a class="fancybox" rel="album-'+i+'" title="'+jtem.name+'" href="'+jtem.source+'"><img src="'+jtem.picture+'" alt=""/></a>');}
 								else { photos.push('<a class="fancybox" rel="album-'+i+'" href="'+jtem.source+'"><img src="'+jtem.picture+'" alt=""/></a>'); }
 						});		
-						albums.push('<div class="album">'+photos.join('')+'<h3>'+item.name+'</h3></div>');
-						$("#albums").html(albums.join(''));
+						albums.push(photos.join('')+'<h3>'+item.name+'</h3>');
+						$(".album-"+item.id).html(albums.join(''));
 						$(".album a").fancybox({
 								'transitionIn'	:	'elastic',
 								'transitionOut'	:	'elastic',
@@ -29,9 +36,11 @@ $(document).ready(function() {
 								'speedOut'		:	200, 
 								'overlayShow'	:	false
 							});
+						a++;
 					}
-				});
-			});
+				}); //fin du second Ajax
+				b++
+			}); // Fin du premier each
 		}
 	});
 	
